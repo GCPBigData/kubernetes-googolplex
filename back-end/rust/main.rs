ecdd5c22f3f386c9b2fb56603174fd79fddd87cc
jr <web2ajax@gmail.com>
2021-03-20 14:04:04 -0300

    rust

--- back-end/rust/main.rs
+++ back-end/rust/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    
+    let _nome = "NIETZSCHE GURDJIEFF";
+    
+    let _valor128f: i128 = 11111111111111111111111111;
+    let _valor128: i128 = 666666666666666666666666;
+    let _soma: i128 = _valor128 + _valor128f;
+
     println!("AULA DE KUBERNETES !");
+    println!("{}", _soma);
 }
